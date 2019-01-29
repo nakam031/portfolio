415eaa732463f070c6672a59c3ba54f7ce99a9ad
Hitomi Nakamura <nakam031@ad.umn.edu>
2019-01-29 13:29:37 -0600

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,34 +1,12 @@
 // Sticky navbar
 // =========================
 
-$(document).ready(function() {
-    // Custom function which toggles between sticky class (is-sticky)
-    var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
-        var stickyHeight = sticky.outerHeight();
-        var stickyTop = stickyWrapper.offset().top;
-        if (scrollElement.scrollTop() >= stickyTop) {
-            stickyWrapper.height(stickyHeight);
-            sticky.addClass("is-sticky");
-        }
-        else {
-            sticky.removeClass("is-sticky");
-            stickyWrapper.height('auto');
-        }
-    };
-
-    // Find all data-toggle="sticky-onscroll" elements
-    $('[data-toggle="sticky-onscroll"]').each(function () {
-        var sticky = $(this);
-        var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
-        sticky.before(stickyWrapper);
-        sticky.addClass('sticky');
-
-        // Scroll & resize events
-        $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
-            stickyToggle(sticky, stickyWrapper, $(this));
-        });
-
-        // On page load
-        stickyToggle(sticky, stickyWrapper, $(window));
-    });
+$(window).scroll(function () {
+  console.log($(window).scrollTop())
+  if ($(window).scrollTop() > 63) {
+    $('#mainNavbar').addClass('navbar-fixed');
+  }
+  if ($(window).scrollTop() < 64) {
+    $('#mainNavbar').removeClass('navbar-fixed');
+  }
 });
